17d5034cef5d1e85287aa576b069684f3955b3d8
Eugene Jahn <eugenejahnjahn@gmail.com>
2020-10-18 11:52:11 +0800

    double text lmao

--- js/danmaku.js
+++ js/danmaku.js
@@ -17,8 +17,11 @@
 //   ['happy Hack', 8],
 //   ['hello Hack PSU', 17],
 // ];
+
+// require "ReadFirebase.js"
+
 var comments = {
-  '00:21': [
+  '00:00:01': [
     'this is from write again',
     'this is from write again',
     'this is from write again',
@@ -28,8 +31,80 @@ var comments = {
     'this is from write again',
     'this is from write again',
   ],
-  '00:23': ['new test', 'new test2', 'new test696996'],
+  '00:00:03': ['new test', 'new test2', 'new test696996'],
 };
+
+
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyCZ4aKEj0k1tOXw9C1fbhgbs5lXU7GQMQ0',
+  authDomain: 'dubhacks-292821.firebaseapp.com',
+  databaseURL: 'https://dubhacks-292821.firebaseio.com',
+  projectId: 'dubhacks-292821',
+  storageBucket: 'dubhacks-292821.appspot.com',
+  messagingSenderId: '186332677431',
+  appId: '1:186332677431:web:94290b053b2adf92b7c6b2',
+  measurementId: 'G-VGGMLBTLY1',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+var db = firebase.firestore();
+
+function writeData(classId, timeStamp, comment) {
+  const usersRef = db.collection('Classes').doc(classId);
+  usersRef.get().then((docSnapshot) => {
+    // makes sure class exists
+    let dataObj = {};
+    if (docSnapshot.exists) {
+      var data = docSnapshot.data();
+      if (data[timeStamp] == null || data[timeStamp] == '') {
+        dataObj[timeStamp] = [comment];
+        usersRef.update(dataObj);
+        return true;
+      } else {
+        dataObj[timeStamp] = data[timeStamp];
+        dataObj[timeStamp].push(comment);
+        console.log(dataObj);
+        usersRef.update(dataObj);
+        return true;
+      }
+    } else {
+      let dataObj = {};
+      dataObj[timeStamp] = [comment];
+      usersRef.set(dataObj);
+      return true;
+    }
+  });
+}
+
+function getURL(){
+  var href = location.href.split('/');
+
+  return href[5].split('?')[0];
+}
+
+console.log(getURL());
+
+async function readData(classId) {
+  const data = await db
+    .collection('Classes')
+    .doc(classId)
+    .get()
+    .then((res) => {
+      return res.data();
+    });
+  return data;
+}
+
+async function callAsync() {
+  comments = await readData(getURL());
+}
+
+callAsync();
+
+
+
 
 var i = 0;
 flag = false;
@@ -48,12 +123,13 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 });
 
 function writeDataToFireBase(comment) {
+  insertComment(comment);
   var liveTime = getVideoTime();
   console.log(liveTime);
-  var href = location.href.split('/');
-  // if (href.length > )
-  console.log(href[5]);
+  var href = getURL();
+  console.log(href);
   console.log(comment);
+  writeData(href, liveTime,comment);
 }
 
 // var oldURL = location.href;
@@ -69,8 +145,8 @@ function writeDataToFireBase(comment) {
 // Can let Alvin and Eugene check
 function checkLiveTime() {
   var liveTime = getVideoTime(); // They will give us
-  if (comments[liveTime] !== null) {
-    insertCommments(comments[liveTime]);
+  if (comments[liveTime]) {
+    insertComments(comments[liveTime], liveTime);
   }
 }
 
@@ -97,21 +173,24 @@ function getVideoTime() {
   return timestamp;
 }
 
-function insertCommments(commentsAtTime) {
+function insertComments(commentsAtTime, liveTime) {
+  console.log("insertsComments")
+  console.log(commentsAtTime)
   for (j = 0; j < commentsAtTime.length; j++) {
-    insertCommment(comments[liveTime][j]);
+    insertComment(comments[liveTime][j]);
   }
 }
-function displayCommment(commentText) {
+function insertComment(commentText) {
   var commentSpan = document.createElement('span');
   var t = document.createTextNode(commentText);
   commentSpan.appendChild(t);
+  var color = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6)
   $(commentSpan).css({
     position: 'absolute',
     width: 'auto',
     'max-width': '250px',
     'white-space': 'nowrap',
-    color: '0xffffff',
+    color: color,
     height: '20px',
     left: '0',
     top: '0',
@@ -124,13 +203,16 @@ function displayCommment(commentText) {
 
   // var obj = commentSpan;
   var speed = Math.floor(Math.random() * 3) + 2;
-  var delay = 10;
+  var delay = 7;
   var videoDiv = document.getElementsByClassName('player-view')[0];
   var videoCoordinates = videoDiv.getBoundingClientRect();
   var xPos = videoCoordinates.right - commentSpan.offsetWidth;
   var yPos = Math.floor(
-    Math.random() * videoDiv.offsetHeight * 0.2 + videoCoordinates.top
-  );
+    (Math.random() * videoDiv.offsetHeight  + videoCoordinates.top)
+  ) - 25;
+  if(yPos < videoCoordinates.top ){
+    yPos = videoCoordinates.top;
+  }
 
   function rollMethod() {
     var minX = videoCoordinates.left;
@@ -233,3 +315,7 @@ function displayer() {
 //window.onload = displayCommment("Comment is the best thing");
 
 var intervalID = window.setInterval(checkLiveTime, 500);
+
+
+
+
